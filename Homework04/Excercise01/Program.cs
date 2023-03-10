ab12b68a8557f5fa4e4fcb74f1ee4c65bcc5c6ca
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2023-03-10 11:31:56 +0100

    Adding homework excercises

---
+++ Homework04/Excercise01/Program.cs
@@ -0,0 +1,42 @@
+﻿//Таск 01-Take one string from the input and print its last 5 characters.
+//Console.WriteLine("Please enter a word: ");
+//string userInput = Console.ReadLine();
+//string newString = userInput.Substring(userInput.Length - 5);
+//Console.WriteLine(newString);
+
+
+//Task 02-Take a sentence as input and print its words.
+
+//Console.WriteLine("Please enter a sentence:");
+//string splitToWordsArray = Convert.ToString(Console.ReadLine());
+//string[] newSplittedArray = splitToWordsArray.Split(" ");
+//for (int i = 0; i < newSplittedArray.Length; i++)
+//{
+//    Console.WriteLine(newSplittedArray[i]);
+//}
+
+
+
+//Task 03-Create a function that takes a number as input. This method should return the sum of the digits in the number.
+
+Console.WriteLine("Please enter a number (multiple digits)");
+bool userNumberInput = int.TryParse(Console.ReadLine(), out int number);
+int[] numberDigitsArray = number.ToString(); 
+
+for (int i = 0; i < numberDigitsArrayth; i++)
+{ 
+    Console.WriteLine(numberDigitsArray[i]);
+}
+
+
+
+//int sumOfNumberDigits (int number)
+//{
+
+//}
+
+
+
+
+
+Console.ReadLine();
