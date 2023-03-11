7b936178073978c2d323e883a768e88cb2a68115
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2023-03-11 19:46:30 +0100

    Adding Task04

--- Homework04/Excercise01/Program.cs
+++ Homework04/Excercise01/Program.cs
@@ -19,22 +19,81 @@
 
 //Task 03-Create a function that takes a number as input. This method should return the sum of the digits in the number.
 
-int PrintNumberDigits ()
+//void SumNumberDigits ()
+//{
+//    int sum = 0;
+//    Console.WriteLine("Please enter a number (multiple digits)");
+//    int userNumberInput = Convert.ToInt32(Console.ReadLine());
+//    while (userNumberInput != 0)
+//    {
+//        sum += userNumberInput % 10;
+//        userNumberInput /= 10;
+//    }
+//    Console.WriteLine("The sum of the digits is: " + sum);
+//}
+
+//SumNumberDigits();
+
+//Task 4
+//Make a method called AgeCalculator
+//The method will have one input parameter, your birthday date
+//The method should return your age
+//Show the age of a user after he inputs a date
+//Note: take into consideration if the birthday is today, after or before today
+
+
+
+int AgeCalculator(DateTime dateOfBirth)
 {
-    int userNumberInput = 0;
-    int sum = 0;
-    Console.WriteLine("Please enter a number (multiple digits)");
-    userNumberInput = Convert.ToInt32(Console.ReadLine());
-    int[] numberDigitsArray = userNumberInput.ToString();
-    for (int i = 0; i < numberDigitsArrayth; i++)
-    {
-        sum += numberDigitsArray[i];
-    }
+    int age = 0;
+    age = DateTime.Today.Year - dateOfBirth.Year;
+    return age;
+    
 }
 
+DateTime birthday1 = new DateTime(1996, 4, 23);
+DateTime birthday2 = new DateTime(2000, 11, 25);
+DateTime birthday3 = new DateTime(1971, 4, 13);
+DateTime birthday4 = new DateTime(1973, 8, 4);
+DateTime birthdayToday = DateTime.Now;
+Console.WriteLine($"You are {AgeCalculator(birthday1)} years old!");
+Console.WriteLine($"You are {AgeCalculator(birthday2)} years old!");
+Console.WriteLine($"You are {AgeCalculator(birthday3)} years old!");
+Console.WriteLine($"You are {AgeCalculator(birthday4)} years old!");
 
 
 
+
+
+
+//DateTime date = new DateTime();
+//Console.WriteLine(date);
+
+//DateTime currentDate = DateTime.Now;
+//Console.WriteLine(currentDate);
+
+//DateTime universalDateTime = DateTime.UtcNow;
+//Console.WriteLine(universalDateTime);
+
+
+//DateTime currentDay = DateTime.Today;
+//Console.WriteLine(currentDay);
+//Console.WriteLine(currentDate.Date);
+
+
+//DateTime newDate = new DateTime(1993, 08, 27);
+//Console.WriteLine(newDate);
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
 
 
 
