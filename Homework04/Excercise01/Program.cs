e71492de593d2c10c578e2d3fee07fe3a660959c
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2023-03-12 15:17:19 +0100

    Adding new changes

--- Homework04/Excercise01/Program.cs
+++ Homework04/Excercise01/Program.cs
@@ -66,28 +66,6 @@ Console.WriteLine($"You are {AgeCalculator(birthday4)} years old!");
 
 
 
-//DateTime date = new DateTime();
-//Console.WriteLine(date);
-
-//DateTime currentDate = DateTime.Now;
-//Console.WriteLine(currentDate);
-
-//DateTime universalDateTime = DateTime.UtcNow;
-//Console.WriteLine(universalDateTime);
-
-
-//DateTime currentDay = DateTime.Today;
-//Console.WriteLine(currentDay);
-//Console.WriteLine(currentDate.Date);
-
-
-//DateTime newDate = new DateTime(1993, 08, 27);
-//Console.WriteLine(newDate);
-
-
-
-
-
 
 
 
