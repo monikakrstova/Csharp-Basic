e544fc418a5d8ce00582b9de878fec897cad0406
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2023-03-19 17:19:34 +0100

    Adding task01-Homework05

---
+++ Homework05/Homework05-Task01/Models/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_Task01.Models
+{
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetPersonDetails()
+        {
+            return $"This is {FirstName} {LastName} and is {Age} years old!";
+        }
+    }
+}
---
+++ Homework05/Homework05-Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Homework05_Task01.Models;
+
+namespace Homework05_Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name, last name and age:");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            bool ageInput = int.TryParse(Console.ReadLine(), out int age);
+
+            Human human = new Human() { FirstName = firstName, LastName = lastName, Age = age};
+
+            Console.WriteLine(human.GetPersonDetails());
+        }
+    }
+}
